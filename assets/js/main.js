b69f4caaad2ff957e65ab6e2caf21b5e5414ab33
Tommaso Figlia <tommasofiglia@gmail.com>
2021-01-20 10:51:24 +0100

    Some js added to navbar links. Main's first section created.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,26 @@
 let app = new Vue({
   // options object
   el: "#app",
+  data:{
+    navLinks:[
+      {
+        title: "home"
+      },
+      {
+        title: "pages"
+      },
+      {
+        title: "courses"
+      },
+      {
+        title: "features"
+      },
+      {
+        title: "blog"
+      },
+      {
+        title: "shop"
+      }
+    ]
+  }
 });
